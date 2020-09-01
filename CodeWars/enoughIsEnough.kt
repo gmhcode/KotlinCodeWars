c1406319b95dc1a05be2eef3711f692fa7e7a9d7
gmhcode <greg.hughes@meritagepos.com>
2020-08-31 19:38:15 -0600

    e

--- CodeWars/enoughIsEnough.kt
+++ CodeWars/enoughIsEnough.kt
@@ -28,6 +28,15 @@ fun main() {
 //        println(dict.keys)
         return array.toIntArray()
     }
+
+//Best
+//   fun deleteNth(elements: IntArray, n: Int): IntArray {
+//         val counts = mutableMapOf<Int, Int>()
+//         fun countFilter(x: Int) = counts.compute(x, { _, count -> count?.plus(1) ?: 1 })!! <= n
+//         return elements.filter { countFilter(it) }.toIntArray()
+//     }
+
+
 //Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motive usually repeats. He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will only sit during the session if they show the same motive at most N times. Luckily, Alice and Bob are able to encode the motive as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
 //
 //Task
